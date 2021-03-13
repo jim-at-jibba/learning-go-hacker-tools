7eb975fe3a76566e1af3a2239b05e50096cb8fd7
James Best <jim@justjibba.net>
2021-03-13 05:53:23 +0000

    Add simple tcp scanner

---
+++ tcp_scanner/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"net"
+)
+
+func main() {
+	run()
+}
+
+func run() {
+	IP := "scanme.nmap.org"
+	Port := "80"
+
+	address := IP + ":" + Port
+	connection, err := net.Dial("tcp", address)
+
+	if err == nil {
+		fmt.Println("[+] Connection established...", connection.RemoteAddr().String())
+	}
+}
