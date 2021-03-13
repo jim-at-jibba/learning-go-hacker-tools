344a94c7799450cf62631282bfd036a3d3623ab1
James Best <jim@justjibba.net>
2021-03-13 06:19:56 +0000

    Add go rotuine to improve scan speed

--- tcp_scanner/main.go
+++ tcp_scanner/main.go
@@ -3,22 +3,37 @@ package main
 import (
 	"fmt"
 	"net"
+	"sync"
 )
 
 func main() {
 	run()
 }
 
-func run() {
+func ScanPort(port int, wg *sync.WaitGroup) {
+	defer wg.Done()
 	IP := "scanme.nmap.org"
+	address := fmt.Sprintf(IP+":%d", port)
+	connection, err := net.Dial("tcp", address)
 
+	if err != nil {
+		return
+	}
+	fmt.Printf("[+] Connection established...PORT %v - IP: %v\n", port, connection.RemoteAddr().String())
+	connection.Close()
+}
+func run() {
+
+	// wg = waitgroup
+	// waitgroups help us keep the main function from
+	//  closing before our go routines
+	// have finihsed
+	var wg sync.WaitGroup
 	for i := 1; i < 100; i++ {
-		address := fmt.Sprintf(IP+":%d", i)
-		connection, err := net.Dial("tcp", address)
-
-		if err == nil {
-			fmt.Printf("[+] Connection established...PORT %v - IP: %v\n", i, connection.RemoteAddr().String())
-		}
+		wg.Add(1)
+		go ScanPort(i, &wg)
 	}
 
+	// waits until wg counter is at zero
+	wg.Wait()
 }
